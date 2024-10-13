96da811a3196f07fd7abcfc3a6398ef2f4836777
Sadiq Quliyev <sadiq000k@gmail.com>
2024-10-13 18:41:00 +0400

    Proje dosyası ekle.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	/*int aci = 0;
+	while (aci<256)
+	{
+		cout<<aci << " :" << (char)aci << endl;
+		aci++;
+	}*/
+	/*int u;
+	cout<< "ulduz sayi : ";
+	cin>>u;
+	int say = 0;
+	while (say < u) {
+		cout << "*";
+		say++;
+	}
+	cout << endl;*/
+	//int num = 0;
+	//while (num< 50)
+	//{
+	//	if (num%2== 0)
+	//		cout << num << endl;
+	//	num++;
+	//}
+	//int num;
+	//cout << "Eded daxil edin: ";
+	//cin >> num;
+	//int num2;
+	//cout << "Eded daxil edin: ";
+	//cin >> num2;
+	//int tek = 1;
+	//int cut = 0;
+	//while (num < num2)
+	//{
+	//	if (num % 2 == 0)
+	//		cut += num;
+	//	else
+	//		tek *= num;
+	//	num++;
+	//}
+	//cout << "Cut reqemlerin cemini:" << cut << endl;
+	//cout << "Tek reqemlerin hasili:" << tek << endl;
+}
